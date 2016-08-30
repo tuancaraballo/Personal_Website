3084f5f417477c2d90159d8ad3347c209c1bf6da
tuancaraballo <tranc2606@gmail.com>
2016-08-30 11:43:19 -0700

    Starting my personal website

---
+++ home-controller.js
@@ -0,0 +1,6 @@
+ app.controller('home-controller', ['$scope','$location', '$route', '$window',
+        function ($scope, $location, $route, $window) {
+            console.log("got to HOME Controller");
+
+                  
+}]);
