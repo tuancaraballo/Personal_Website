229129460d2cecd1d9e34ca5138e8325c486099b
tuancaraballo <tranc2606@gmail.com>
2016-08-30 17:35:16 -0700

    finish

--- mainController.js
+++ mainController.js
@@ -4,16 +4,6 @@
     var myapp = angular.module('myWebApp', ['ngRoute', 'ngMaterial']);
 
 
-
-/*
- when('/activityFeed', {
-                    templateUrl: 'components/activity-feed/activityFeed.html',
-                    controller: 'activityFeed-Controller'
-                }).
-
-*/
-
-
     myapp.config(['$routeProvider',
         function ($routeProvider) {
             $routeProvider.
@@ -38,9 +28,9 @@
         function ($scope, $route, $http, $window, $location) {
             console.log("got to Controller");
 
-            $scope.first_name = 'Nicola Rider';
+            $scope.first_name = 'Tuan Anh Tran Caraballo';
             $scope.main = {};
-            $scope.main.title = 'Nicola Rider';
+            $scope.main.title = 'Tuan Anh Tran Caraballo';
             var loc = window.location.pathname;
             console.log("Current directory " + loc);
 
@@ -53,7 +43,6 @@
          var myButton1 = angular.element(document.querySelector('#'+previousButton) );
          myButton1.removeClass('buttons_style2');
          myButton1.addClass('buttons_style');
-         // myButton.css('color','#4D4D4D');
        }
 
        var changeCurrentElementColor = function(){
