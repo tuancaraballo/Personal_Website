e5776185070424abbbca8701a14e8d0fe7605be6
tuancaraballo <tranc2606@gmail.com>
2016-09-01 08:18:58 -0700

    understood ui-bootstrap carousel, just implemented a basic version with puppies photos :-)

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -106,12 +106,29 @@
         function ($scope, $location, $route, $window) {
             console.log("got to Project Controller");
 
-            $scope.myInterval = 5000;
-            $scope.noWrapSlides = false;
-            $scope.active = 0;
-            var slides = $scope.slides = [];
-            var currIndex = 0;
 
+            $scope.theLInk = "images/pup2.png";
+            $scope.myInterval = 2000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  var slides = $scope.slides = [];
+  var currIndex = 0;
+
+
+
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: 'images/pup' + currIndex+ '.png',
+      text: ['Puppy1','Puppy2','Puppy3'][slides.length % 3],
+      id: currIndex++
+    });
+  };
+
+
+  for (var i = 0; i < 3; i++) {
+    $scope.addSlide();
+  }
                  
     }]);
 
--- home-controller.js
+++
@@ -1,6 +0,0 @@
- app.controller('home-controller', ['$scope','$location', '$route', '$window',
-        function ($scope, $location, $route, $window) {
-            console.log("got to HOME Controller");
-
-                  
-}]);
