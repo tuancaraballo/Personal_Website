9a15bfda9e769db00354c822e923b6de1a8e6d85
tuancaraballo <tranc2606@gmail.com>
2016-09-01 11:41:51 -0700

    finished projects section

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -31,9 +31,9 @@
         function ($scope, $route, $http, $window, $location) {
             console.log("got to Controller");
 
-            $scope.first_name = 'Tuan Anh Tran Caraballo';
+            $scope.first_name = 'Nesta Robert Marley';
             $scope.main = {};
-            $scope.main.title = 'Tuan Anh Tran Caraballo';
+            $scope.main.title = 'Nest Robert Marley';
 
             $scope.myTwitter = "https://www.twitter.com/tuan26062";
             $scope.myGitHub = "https://www.github.com/tuancaraballo";
@@ -106,29 +106,26 @@
         function ($scope, $location, $route, $window) {
             console.log("got to Project Controller");
 
-
-            $scope.theLInk = "images/pup2.png";
             $scope.myInterval = 2000;
-  $scope.noWrapSlides = false;
-  $scope.active = 0;
-  var slides = $scope.slides = [];
-  var currIndex = 0;
+            $scope.noWrapSlides = false;
+            $scope.active = 0;
+  
+            var slides = $scope.slides = [];
+            var currIndex = 0;
 
 
-
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    slides.push({
-      image: 'images/pup' + currIndex+ '.png',
-      text: ['Puppy1','Puppy2','Puppy3'][slides.length % 3],
-      id: currIndex++
-    });
-  };
-
-
-  for (var i = 0; i < 3; i++) {
-    $scope.addSlide();
-  }
+        $scope.addSlide = function() {
+            var newWidth = 600 + slides.length + 1;
+            slides.push({
+                image: 'images/pup' + currIndex+ '.png',
+                text: ['Puppy1','Puppy2','Puppy3'][slides.length % 3],
+                id: currIndex++
+            });
+        };
+        
+        for (var i = 0; i < 3; i++) {
+            $scope.addSlide();
+        }
                  
     }]);
 
