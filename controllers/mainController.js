d1c13ff88c5896836644bb92cc372f5588a7da0a
tuancaraballo <tranc2606@gmail.com>
2016-09-02 23:09:38 -0700

    moving all resume data to the controller, this way I avoid repeating extra html code, making it lean and beautiful code

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -31,9 +31,9 @@
         function ($scope, $route, $http, $window, $location) {
             console.log("got to Controller");
 
-            $scope.first_name = 'Nesta Robert Marley';
+            $scope.first_name = 'Tuan Anh Tran Caraballo';
             $scope.main = {};
-            $scope.main.title = 'Nest Robert Marley';
+            $scope.main.title = 'Tuan Anh Tran Caraballo';
 
             $scope.pageClass = "containerHome";
 
@@ -137,6 +137,63 @@
      myapp.controller('resume-controller', ['$scope','$location', '$route', '$window',
         function ($scope, $location, $route, $window) {
             console.log("got to Resume Controller");
+
+            var jobsList =[
+                    {
+                              title: "Home Depot",
+                        description: "I'm a Paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story about yourself."
+                    },
+                    {
+                              title: "Tutor",
+                        description: "I'm a Paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story about yourself."
+                    },
+                    {
+                              title: "Medical School",
+                        description: "I'm a Paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story about yourself."
+                    }
+
+               ];
+
+            var educationList= [
+                    {
+                             title: "Stanford University",
+                       description:  'Stanford University --- 2018'
+                    }
+            ];
+
+            var publicServiceList = [
+                 {
+                             title: "Cuba Emprende",
+                       description: "I'm a Paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story about yourself."
+                },
+                 {
+                             title: "Uniting Neighborhoods",
+                       description: "I'm a Paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to write more. Tell a story about yourself."
+                }
+
+            ];  
+
+
+               $scope.resumeDataSet = [
+                    {
+                        sectionTitle: "Education",
+                                data: educationList
+
+                    },
+                    {
+                        sectionTitle: "Work Experience",
+                                data: jobsList
+
+                    },
+                    {
+                        sectionTitle: "Public Service",
+                                data: publicServiceList
+                    }
+
+               ];
+
+
+               
 
             
                  
