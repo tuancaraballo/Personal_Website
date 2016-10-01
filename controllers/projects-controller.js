f2b7e0396bce40b0ef9a7bd6508c05ef548d9483
tuancaraballo <tranc2606@gmail.com>
2016-09-30 21:44:37 -0700

    finished Unagi's image resize and description

--- controllers/projects-controller.js
+++ controllers/projects-controller.js
@@ -4,25 +4,71 @@
 
             $rootScope.linux = false;
             $scope.pageClass = '.containerProjects';
-            $scope.myInterval = 10000;
-            $scope.noWrapSlides = false;
-            $scope.active = 0;
+           
+
+           
+            $scope.unagiInterval = 10000;
+            $scope.unagiNoWrapSlides = false;          
+            $scope.unagiActive = 0;
   
-            var slides = $scope.slides = [];
-            var currIndex = 0;
+
+  //--- Heap Slides for carousel  --- 
+            $scope.heapInterval = 10000;
+            $scope.heapNoWrapSlides = false;
+            $scope.heapActive = 0;
+            var heapSlides = $scope.heapSlides = [];
+            var heapIndex = 0;
 
 
-        $scope.addSlide = function() {
-            var newWidth = 600 + slides.length + 1;
-            slides.push({
-                image: 'images/heap' + currIndex+ '.png',
-                // text: ['Puppy1','Puppy2','Puppy3'][slides.length % 3],
-                id: currIndex++
-            });
-        };
+            $scope.addHeap = function() {
+                 var newWidth = 600 + heapSlides.length + 1;
+                 heapSlides.push({
+                     image: 'images/heap' + heapIndex+ '.png',
+                 // text: ['Puppy1','Puppy2','Puppy3'][slides.length % 3],
+                     id: heapIndex++
+                 });
+            };
+
+            for (var i = 0; i < 3; i++) {
+                $scope.addHeap();
+            }
+
+
+ //--- Unagi Slides for carousel ---
+           $scope.unagiInterval = 10000;
+           $scope.unagiNoWrapSlides = false;          
+           $scope.unagiActive = 0;
+           var unagiSlides = $scope.unagiSlides = [];
+           var unagiIndex = 0;
+
+
+            $scope.addUnagi = function() {
+                 var newWidth = 600 + unagiSlides.length + 1;
+                 unagiSlides.push({
+                     image: 'images/unagi' + unagiIndex+ '.png',
+                 // text: ['Puppy1','Puppy2','Puppy3'][slides.length % 3],
+                     id: unagiIndex++
+                 });
+            };  
+
+            for (var i = 0; i < 7; i++) {
+                $scope.addUnagi();
+            }         
+
+        // currIndex
+
+        // $scope.addUnagi = function() {
+        //     var newWidth = 600 + slides.length + 1;
+        //     slides.push({
+        //         image: 'images/heap' + currIndex+ '.png',
+        //         // text: ['Puppy1','Puppy2','Puppy3'][slides.length % 3],
+        //         id: currIndex++
+        //     });
+        // };
+
+
         
-        for (var i = 0; i < 3; i++) {
-            $scope.addSlide();
-        }
+
+        
                  
     }]);
